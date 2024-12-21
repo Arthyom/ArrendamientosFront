0e6f9d12bcab680c75df8a7d5dcadc161dc658e8
Alfredo Gonzalez Gaviña <alfredo.gonzalez@vientodigital.com>
2024-12-20 21:18:56 -0600

    Changing port to listen locally

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   ]
 };
 
-// export const baseUrl: string = 'https://localhost:7205/api/'
-export const baseUrl: string = 'http://www.arrendamiento.front.alfredowflsyopollo.us/api/'
+ export const baseUrl: string = 'http://localhost:5000/api/'
+// export const baseUrl: string = 'http://www.arrendamiento.front.alfredowflsyopollo.us/api/'
 
 
